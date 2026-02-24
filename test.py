bf313b95f2382718b32c8256f371ab1b3f6d7b53
dittoandrewsmala <85013963+dittoandrewsmala@users.noreply.github.com>
2026-02-24 20:12:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,29 @@
-from datetime import datetime, timedelta
+import threading
+from flask import Flask, request
+import hashlib
+import requests
+from logger import Logger
+import config
+import time
 
-today = datetime.today()
+logger = Logger()
+app = Flask(__name__)
+auth_data = {}
 
-# Tuesday = 1 (Monday=0)
-days_ahead = 1 - today.weekday()
+def start_flask():
+        """Run Flask in background to receive request_code from redirect URL"""
+        app.run(host="0.0.0.0", port=8080)
 
-if days_ahead < 0:
-    days_ahead += 7
+ # Start Flask in background thread
+threading.Thread(target=start_flask, daemon=True).start()
 
-next_tuesday = today + timedelta(days=days_ahead)
+# Provide user with the authorization URL
+logger.printD(f"Please open this URL in a browser to login: {config.AUTH_URL}")
 
-expiry = next_tuesday.strftime("%d%b%Y").upper()
+# Wait for callback to receive request_code
+logger.printD("Waiting for request_code from Flattrade callback...")
+while "request_code" not in auth_data:
+    time.sleep(1)
 
-print(expiry)
+request_code = auth_data["request_code"]
+logger.printD(f"Received request_code: {request_code}")
