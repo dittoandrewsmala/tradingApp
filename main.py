c90818cd481978498d5d9b56e08c9f4e047ed1b3
Ditto Andrews <ditto.andrews@nestgroup.net>
2026-06-10 10:02:41 +0530

    update new stratergy

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ def on_tick(price):
     candle = builder.update(price)
     if candle:
         signal=strategy.on_candle(candle, datetime.now(pytz.timezone("Asia/Kolkata")))
+        print("Signal:", signal)
     
         
     ## receving signal 
@@ -67,7 +68,7 @@ def on_tick(price):
     
     
      
-    if  signal !=None and ("SELL" in signal or "BUY" in signal) and not isTradeActive and current_time >= time(9, 30):
+    if  signal !=None and signal and signal.get("action") in ["BUY", "SELL"]  and not isTradeActive and current_time >= time(9, 30):
         trade.session_token=broker.session
         print("current index value: "+ str(lotIndex))
         if "BUY" in signal:
