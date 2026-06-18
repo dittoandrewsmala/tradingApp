b88fe0424be55b33ab9834cfb137fcc0054b1875
Ditto Andrews <ditto.andrews@nestgroup.net>
2026-06-18 15:03:06 +0530

    updates the code

--- config.py
+++ config.py
@@ -38,6 +38,7 @@ PAPER_MODE = False
 
 CAPITAL = 20000
 RISK_PERCENT = 1
+CANDLE_DIFF= 1.5
 
 stop_target_counter=1
 
--- main.py
+++ main.py
@@ -85,10 +85,10 @@ def on_tick(price, volume,open,low,high,close):
         
         condition = None
         if  last_candle["close"] > first_candle["close"] and last_candle["close"] > last_candle_2["close"] and last_candle_2["close"] > last_candle_3["close"] and last_candle_3["close"] > last_candle_4["close"] and last_candle_4["close"] > last_candle_5["close"]:
-            if last_candle["close"] - last_candle_2["close"] >=1 and last_candle_2["close"] - last_candle_3["close"] >=1 and last_candle_3["close"] - last_candle_4["close"] >=1 and last_candle_4["close"] - last_candle_5["close"] >=1:
+            if last_candle["close"] - last_candle_2["close"] >=config.CANDLE_DIFF and last_candle_2["close"] - last_candle_3["close"] >=config.CANDLE_DIFF and last_candle_3["close"] - last_candle_4["close"] >=config.CANDLE_DIFF and last_candle_4["close"] - last_candle_5["close"] >=config.CANDLE_DIFF:
                 condition = "BUY"
         elif  last_candle["close"] < first_candle["close"] and last_candle["close"] < last_candle_2["close"] and last_candle_2["close"] < last_candle_3["close"] and last_candle_3["close"] < last_candle_4["close"] and last_candle_4["close"] < last_candle_5["close"]:
-            if last_candle["close"] - last_candle_2["close"] >=-1 and last_candle_2["close"] - last_candle_3["close"] >=-1 and last_candle_3["close"] - last_candle_4["close"] >=-1 and last_candle_4["close"] - last_candle_5["close"] >=-1:
+            if (last_candle_2["close"] - last_candle["close"] >= config.CANDLE_DIFF and last_candle_3["close"] - last_candle_2["close"] >= config.CANDLE_DIFF and last_candle_4["close"] - last_candle_3["close"] >= config.CANDLE_DIFF and last_candle_5["close"] - last_candle_4["close"] >= config.CANDLE_DIFF):
                 condition = "SELL"
         if condition is None:
             return
