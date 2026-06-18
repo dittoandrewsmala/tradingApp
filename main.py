de8d4734bcc82ed6ede387c5c24e2f5691b8244b
Ditto Andrews <ditto.andrews@nestgroup.net>
2026-06-18 11:28:31 +0530

    updates the code

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def on_tick(price, volume,open,low,high,close):
             if last_candle["close"] - last_candle_2["close"] >=1 and last_candle_2["close"] - last_candle_3["close"] >=1 and last_candle_3["close"] - last_candle_4["close"] >=1 and last_candle_4["close"] - last_candle_5["close"] >=1:
                 condition = "BUY"
         elif  last_candle["close"] < first_candle["close"] and last_candle["close"] < last_candle_2["close"] and last_candle_2["close"] < last_candle_3["close"] and last_candle_3["close"] < last_candle_4["close"] and last_candle_4["close"] < last_candle_5["close"]:
-            if last_candle["close"] - last_candle_2["close"] >=1 and last_candle_2["close"] - last_candle_3["close"] >=1 and last_candle_3["close"] - last_candle_4["close"] >=1 and last_candle_4["close"] - last_candle_5["close"] >=1:
+            if last_candle["close"] - last_candle_2["close"] >=-1 and last_candle_2["close"] - last_candle_3["close"] >=-1 and last_candle_3["close"] - last_candle_4["close"] >=-1 and last_candle_4["close"] - last_candle_5["close"] >=-1:
                 condition = "SELL"
         if condition is None:
             return
