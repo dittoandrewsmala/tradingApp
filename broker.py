8e0bf0a0749c94054be833f30c472d5e52eb11d4
Ditto Andrews <ditto.andrews@nestgroup.net>
2026-07-09 10:10:51 +0530

    updates the code

--- broker.py
+++ broker.py
@@ -125,12 +125,12 @@ class Broker:
         token = next(item["token"] for item in data["values"] if item["idxname"] == "Nifty 50")
         return  token
     
-    def place_order(self, side, lotIndex,symbol,ltp,dif):
+    def place_order(self, side, lotIndex,symbol,ltp,dif,stocktoken):
 
              
         order.session_token = self.session 
         # delegate to Order class instance
-        orderId,profitOrLoss=order.submit_order(side, symbol, lotIndex,ltp,dif)
+        orderId,profitOrLoss=order.submit_order(side, symbol, lotIndex,ltp,dif,stocktoken)
         return orderId,profitOrLoss
 
     
